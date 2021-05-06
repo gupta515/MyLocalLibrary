6552e3edf1e7def6c385860982f239ab4470c879
Gupta <gupta@anywhere.co>
2021-05-06 11:41:19 +0530

    initial commit

---
+++ MyLocalLibrary/MyLocalLibrary/MyLocalLibrary.h
@@ -0,0 +1,18 @@
+//
+//  MyLocalLibrary.h
+//  MyLocalLibrary
+//
+//  Created by Gupta on 06/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLocalLibrary.
+FOUNDATION_EXPORT double MyLocalLibraryVersionNumber;
+
+//! Project version string for MyLocalLibrary.
+FOUNDATION_EXPORT const unsigned char MyLocalLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLocalLibrary/PublicHeader.h>
+
+
---
+++ MyLocalLibrary/MyLocalLibrary/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  MyLocalLibrary
+//
+//  Created by Gupta on 06/05/21.
+//
+
+import Foundation
+
+public class Service {
+    private init () {}
+    
+    public static func getMyLibrary() -> String {
+        return "No Books found"
+    }
+    
+}
